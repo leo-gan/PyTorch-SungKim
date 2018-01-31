8401ebfb38a8b03f5dc59bde0276660bb159125c
leo-gan <leo.gan.57@gmail.com>
2018-01-31 09:05:28 -0800

    Initial commit

---
+++ 03_auto_gradient.py
@@ -0,0 +1,43 @@
+import torch
+from torch.autograd import Variable
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+w = Variable(torch.Tensor([1.0]),  requires_grad=True)  # Any random value
+
+# our model forward pass
+
+
+def forward(x):
+    return x * w
+
+# Loss function
+
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred - y) * (y_pred - y)
+
+def my_loss(y_pred, y):
+    return (y_pred - y)**2
+# Before training
+print("predict (before training)",  4, forward(4).data[0])
+
+# Training loop
+for epoch in range(100):
+    for x, y in zip(x_data, y_data):
+        y_pred = forward(x)
+        l = my_loss(y_pred,y)
+        #l = loss(x, y)
+        l.backward()
+        print("\tgrad: ", x, y, w.grad.data[0])
+        w.data = w.data - 0.01 * w.grad.data
+
+        # Manually zero the gradients after updating weights
+        w.grad.data.zero_()
+
+    print("progress:", epoch, l.data[0])
+
+# After training
+print("predict (after training)",  4, forward(4).data[0])
